3a8ea521890eb3861f3c12a877794fede0e2b10c
Tanvir Rahman <trahman@paradynamix.com>
2022-11-10 16:58:27 +0600

    updated dashboard

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import json
 import pandas as pd
 import numpy as np
 import pydeck as pdk
+import altair as alt
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sns
+import plotly.graph_objects as go
 from streamlit_option_menu import option_menu
 
 st.set_page_config(layout="wide")
@@ -52,6 +57,16 @@ gcod_df_df['month'] = gcod_df_df['startdatetime'].dt.month
 gcod_df_df['date'] = gcod_df_df['startdatetime'].dt.date
 gcod_df_df['hour'] = gcod_df_df['startdatetime'].dt.hour
 gcod_df_df['minute'] = gcod_df_df['startdatetime'].dt.minute
+####### for end time conversion
+gcod_df_df['enddatetime'] = pd.to_datetime(gcod_df_df['properties.endtime'])
+gcod_df_df['endyear'] = gcod_df_df['enddatetime'].dt.year
+gcod_df_df['endmonth'] = gcod_df_df['enddatetime'].dt.month
+gcod_df_df['enddate'] = gcod_df_df['enddatetime'].dt.date
+gcod_df_df['endhour'] = gcod_df_df['enddatetime'].dt.hour
+gcod_df_df['endminute'] = gcod_df_df['enddatetime'].dt.minute
+gcod_group_trip = df
+gcod_group_trip = gcod_group_trip.groupby(['properties.taxiid'])['properties.taxiid'].count()
+gcod_group_trip_top = gcod_group_trip.nlargest(30)
 
 # Explore for Street names ############
 gcod_street_df = df.explode('properties.streetnames')
@@ -72,9 +87,8 @@ top_ten_speed = top_speed_df.nlargest(10, 'counts')
 st.header("Traffic Analysis")
 tab_dashboard, tab_raw = st.tabs(["Dashboard", "Raw Data"])
 with st.container():
-    with st.sidebar:
-        st.subheader("Top 10 Busy Road:")
-        st.write(top_ten_street)
+    #with st.sidebar:
+        #st.subheader("Metrix")
 
     with tab_dashboard:
         st.subheader("Summary:")
@@ -94,23 +108,61 @@ with st.container():
         st.subheader("Traffic On Map:")
         st.map(gcod_df[['lon','lat']], zoom=11, )
 
-        col_dist_ave_speed, col_pichart = st.columns(2)
+        col_dist_ave_speed, col_top_ten, col_pichart = st.columns([1.5, 1.3, 3])
         with col_dist_ave_speed:
-            st.subheader("Ave.Speeed vs Max.Speed vs Distance vs Duration:")
+            st.write("Ave.Speeed vs Max.Speed vs Distance vs Duration:")
             df2 = df
             df2 = pd.DataFrame(df2, columns=['properties.tripid', 'properties.avspeed', 'properties.maxspeed', 'properties.duration'])
             df2.rename(columns = {'properties.tripid':'Trip Id', 'properties.avspeed':'Average Speed', 'properties.maxspeed':'Max Speed', 'properties.duration': 'Duration'}, inplace = True)
             df2 = df2.round(decimals = 2).set_index('Trip Id').nlargest(100, ['Average Speed'])
             st.write(df2)
 
+        with col_top_ten:
+            st.write("Top 10 Busy Road:")
+            st.write(top_ten_street)
+
         with col_pichart:
-            st.subheader("Bar chart:")
+            st.write("Road Engagement:")
             st.bar_chart(top_ten_street)
 
-        st.subheader("Line chart for Ave.Speeed vs Max.Speed vs Distance vs Duration:")
-        df3 = df2.nlargest(100, ['Average Speed'])
-        st.line_chart(df3)
-        st.write(gcod_df_df)
+        col_box_plot, col_line_chart = st.columns(2)
+        with col_line_chart:
+            st.write("Ave.Speeed vs Max.Speed vs Distance vs Duration top 100 records:")
+            df3 = df2.nlargest(100, ['Average Speed'])
+            st.line_chart(df3)
+
+        with col_box_plot:
+            st.write("Trips with Time:")
+            fig = px.box(gcod_df_df, x="hour", y="properties.tripid")
+            # Plot
+            st.plotly_chart(fig, use_container_width=True)
+        
+        col_max_speed, col_min_speed, col_ave_speed = st.columns(3)
+        with col_max_speed:
+            fig = go.Figure(go.Indicator(
+            mode = "gauge+number",
+            value = df['properties.maxspeed'].max(),
+            domain = {'x': [0, 1], 'y': [0, 1]},
+            title = {'text': "Top Speed in Max. Column"}))
+            st.plotly_chart(fig, use_container_width=True)
+
+        with col_min_speed:
+            fig = go.Figure(go.Indicator(
+            mode = "gauge+number",
+            value = df['properties.minspeed'].max(),
+            domain = {'x': [0, 1], 'y': [0, 1]},
+            title = {'text': "Top Speed in Min. Column"}))
+            st.plotly_chart(fig, use_container_width=True)
+
+        with col_ave_speed:
+            fig = go.Figure(go.Indicator(
+            mode = "gauge+number",
+            value = df['properties.avspeed'].max(),
+            domain = {'x': [0, 1], 'y': [0, 1]},
+            title = {'text': "Top Speed in Avg. Column"}))
+            st.plotly_chart(fig, use_container_width=True)
+
+        st.write(gcod_group_trip_top)
 
     with tab_raw:
         st.subheader("Raw Data")
